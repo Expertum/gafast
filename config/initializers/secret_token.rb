4d314efff32602fe14a1f388949001bd170ee467
Artem <expertum.ave@gmail.com>
2015-02-25 12:22:30 +0200

    generate hobo new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gafast::Application.config.secret_token = '0155a6c9a18ba44977b846c5a62651385b43beb6b03c45c1d3f5660e902271da9eaae8b96fe0638b29d67710b6ad66d32bf4ab939e5b20edc604645567c26af4'
