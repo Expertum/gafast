8c1f9ebb66eb3c00685eee9212a3d59beb82363f
Artem <expertum.ave@gmail.com>
2015-04-04 09:23:59 +0300

    changed ver

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-GFS_VERSION = "0.8.1"
+GFS_VERSION = "1.3.0"
 
 EMAIL = "gfs@expertum-ave.com"
 EMAIL_FROM = '"Gration Farm System" <'+EMAIL+'>' unless defined? EMAIL_FROM
