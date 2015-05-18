9c55d35ccbbbde8f34b276253b49cde8fcdd270f
Artem <expertum.ave@gmail.com>
2015-05-18 14:41:33 +0300

    remove 0 when price Optima to codeg

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -44,7 +44,8 @@ class StoragesController < ApplicationController
          list.row(0).concat %w{Код_товара Товар	Производитель Срок_годности Предоплата Признак_НДС Количество} 
          @storages.each_with_index { |storage, i|
               @cena_p = (storage.cena.to_f/(1+(storage.nds.to_f+35)/100)).round(2)
-              list.row(i+1).push storage.codeg.to_s, storage.name, storage.madein, storage.srok, @cena_p.to_s, storage.nds.to_i.to_s, storage.count
+              if storage.pr_name == 'Оптима' then @dd = storage.codeg.to_i.to_s end
+              list.row(i+1).push @dd, storage.name, storage.madein, storage.srok, @cena_p.to_s, storage.nds.to_i.to_s, storage.count
 
          }
         header_format = Spreadsheet::Format.new ({ :weight => :bold, :pattern => 1, :pattern_fg_color => :silver})
